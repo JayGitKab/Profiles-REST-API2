6f165a58dc67a4ddd88c42df234ec32855883270
Kabath <jkabath@radford.edu>
2020-04-19 16:01:51 -0400

    Added viewsets to profiles API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """ Servializers a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+    DOB = serializers.CharField(max_length=8)
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('Hello-viewset', views.HelloViewSet, base_name='Hello-viewset')
+ 
 urlpatterns = [
     path('Hello-view/', views.HelloAPIView.as_view()),
+    path('', include(router.urls))
 ]
